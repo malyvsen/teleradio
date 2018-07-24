4bd0890a76ccb71e65a3ffa048e4623f4128fd19
malyvsen <iceflamecode@gmail.com>
2018-07-24 20:40:07 +0200

    simplified pi_setup

--- pi_setup.sh
+++ pi_setup.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-sudo apt -y install git python3-dev python3-pip
-sudo apt -y install libportaudio0 libportaudio2 libportaudiocpp0 portaudio19-dev
-pip3 install --user pyaudio wit speechrecognition
+sudo apt -y install git python3-dev python3-pip python3-pyaudio
+pip3 install --user wit speechrecognition
 git clone https://github.com/malyvsen/teleradio.git

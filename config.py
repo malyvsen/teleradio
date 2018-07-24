35788c75478fbd884774c8194f485e4f309cdf18
malyvsen <iceflamecode@gmail.com>
2018-07-25 00:03:24 +0200

    proper file handling in wit key file storage

--- config.py
+++ config.py
@@ -1,8 +1,14 @@
 wit_key = None
-with open('./config/wit_key', 'r+') as wit_key_file:
-    wit_key = wit_key_file.read()
-    if len(wit_key) == 0:
-        print('teleradio: enter wit.ai server access token: ', end = '')
-        wit_key = input()
-    wit_key_file.truncate(0)
-    wit_key_file.write(wit_key)
+
+wit_key_path = 'config/wit_key'
+try:
+    with open(wit_key_path, 'r') as wit_key_file:
+        wit_key = wit_key_file.read()
+except FileNotFoundError:
+    pass
+
+if not wit_key:
+    print('teleradio: enter wit.ai server access token: ', end = '')
+    wit_key = input()
+    with open(wit_key_path, 'w') as wit_key_file:
+        wit_key_file.write(wit_key)

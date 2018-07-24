dde50a1bb5fa73e5ce6006d8c27e25353cdf425d
malyvsen <iceflamecode@gmail.com>
2018-07-25 01:09:01 +0200

    neater pi_setup, updated for mimic

--- pi_setup.sh
+++ pi_setup.sh
@@ -1,4 +1,20 @@
-#!/bin/bash
-sudo apt -y install git python3-dev python3-pip python3-pyaudio
-pip3 install --user wit speechrecognition
+# commands to execute on a fresh Raspberry Pi (rigth after running raspi-config and rebooting)
+
+# basic functionality
+sudo apt -y install git python3-dev python3-pip
+
+# clone teleradio
 git clone https://github.com/malyvsen/teleradio.git
+
+# speech recognition
+sudo apt -y install python3-pyaudio
+pip3 install --user wit speechrecognition
+
+# speech synthesis
+git clone https://github.com/MycroftAI/mimic.git
+cd mimic
+./autogen.sh
+./configure --prefix='/usr/local'
+make
+make check
+sudo make install

77e718a1ff773120534ce7b99181f6bb1618d64f
malyvsen <iceflamecode@gmail.com>
2018-07-24 23:49:48 +0200

    use r+ mode for wit key file

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 wit_key = None
-with open('./config/wit_key', 'w+t') as wit_key_file:
+with open('./config/wit_key', 'r+') as wit_key_file:
     wit_key = wit_key_file.read()
     if len(wit_key) == 0:
         print('teleradio: enter wit.ai server access token: ', end = '')

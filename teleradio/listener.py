cecf351508b890c2e63f6c9809b48d0fbf46b09f
malyvsen <iceflamecode@gmail.com>
2018-07-25 16:45:47 +0200

    calibration notification

--- teleradio/listener.py
+++ teleradio/listener.py
@@ -3,14 +3,18 @@ import speech_recognition as sr
 
 from teleradio import log
 from teleradio import nlp
+from teleradio.tts import say
 
 
 def listen():
     r = sr.Recognizer()
     with sr.Microphone(sample_rate = 48000) as source:
+        say('Calibrating noise level. A moment of silence, please.')
         log.update('calibrating noise level')
         r.adjust_for_ambient_noise(source)
         log.update('energy threshold at ' + str(r.energy_threshold))
+
+        say('Ready.')
         while True:
             # obtain audio from the microphone
             log.update('listening for speech')
